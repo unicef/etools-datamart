104515af67fadd9f00967e6da46b3cded6e0084b
sax <s.apostolico@gmail.com>
2018-08-24 20:47:08 +0200

    update docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,25 +1,36 @@
 #!/bin/bash -e
 set -e
 
+mkdir -p /var/datamart/{static,log,conf,run,redis}
+rm -f /var/datamart/run/*
 
-if [ "$@" == "datamart" ];then
-    mkdir -p /var/datamart/{static,log,conf,run,redis}
-    rm -f /var/datamart/run/*
+if [ "$@" == "celery" ];then
+    export START_REDIS="false"
+    export START_DATAMART="false"
+    export START_CELERY="true"
+
+    django-admin db-isready --wait --timeout 60 --sleep 5
+    django-admin db-isready --wait --timeout 300  --sleep 5 --connection etools
+
+    exec supervisord --nodaemon
+elif [ "$@" == "datamart" ];then
 
     django-admin db-isready --wait --timeout 60
     django-admin check --deploy
     django-admin init-setup --all --verbosity 1
-
     django-admin db-isready --wait --timeout 300 --connection etools
-    if [ "$STACK" == "1" ];then
-        exec supervisord --nodaemon
+
+    if [ "$DEVELOPMENT_MODE" == "1" ];then
+        python /code/manage.py runserver 0.0.0.0:8000
     else
-        if [ "$DEVELOPMENT_MODE" == "1" ];then
-            python /code/manage.py runserver 0.0.0.0:8000
-        else
-            gunicorn -b 0.0.0.0:8000 etools_datamart.config.wsgi
-        fi
+        gunicorn -b 0.0.0.0:8000 etools_datamart.config.wsgi
     fi
+elif [ "$@" == "stack" ];then
+    django-admin db-isready --wait --timeout 60
+    django-admin check --deploy
+    django-admin init-setup --all --verbosity 1
+    django-admin db-isready --wait --timeout 300 --connection etools
+    exec supervisord --nodaemon
 else
     exec "$@"
 fi
