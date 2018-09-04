626660431c79527a8e870de1cd7b4bc96d970d48
sax <s.apostolico@gmail.com>
2018-09-04 14:08:19 +0200

    fixes test performance issue

--- tests/_test_lib/settings_test.py
+++ tests/_test_lib/settings_test.py
@@ -1,9 +1,8 @@
 from etools_datamart.config.settings import *  # noqa
 
-#
 CACHES['api']['BACKEND'] = "django.core.cache.backends.dummy.DummyCache"  # noqa
 
-# # Use only one db during tests
+# Use only one db during tests
 DATABASES['etools']['PORT'] = DATABASES['default']['PORT']  # noqa
 DATABASES['etools']['HOST'] = DATABASES['default']['HOST']  # noqa
 DATABASES['etools']['USERNAME'] = DATABASES['default'].get('USERNAME', "")  # noqa
@@ -22,5 +21,5 @@ SECURE_HSTS_SECONDS = 1
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_HTTPONLY = False
 ALLOWED_HOSTS = ['*']
-STATIC_ROOT = os.curdir  # noqa
+STATIC_ROOT = os.path.dirname(__file__)  # noqa
 SESSION_COOKIE_SECURE = False
