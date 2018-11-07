e5832fc31d010e1d6da23c81f1a24de41a70b119
sax <s.apostolico@gmail.com>
2018-11-07 15:00:35 +0100

    updates docker entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,15 @@ mkdir -p /var/datamart/{static,log,conf,run,redis}
 rm -f /var/datamart/run/*
 
 
-if [ "$*" == "celery" ];then
+if [ "$*" == "workers" ];then
+    django-admin db-isready --wait --timeout 60 --sleep 5
+    django-admin db-isready --wait --timeout 300  --sleep 5 --connection etools
+    celery worker -A etools_datamart --loglevel=DEBUG --concurrency=4 --purge --pidfile run/celery.pid
+elif [ "$*" == "beat" ];then
+    celery beat -A etools_datamart --loglevel=DEBUG --pidfile run/celerybeat.pid
+elif [ "$*" == "flower" ];then
+    celery flower -A etools_datamart --port=5555 --loglevel=DEBUG --inspect --basic_auth=${FLOWER_USER}:${FLOWER_PASS}
+elif [ "$*" == "celery" ];then
     export START_REDIS="false"
     export START_DATAMART="false"
     export START_CELERY="true"
