8c08247f2c6aa8f2514ef0fbb72bc5d29a42842f
sax <s.apostolico@gmail.com>
2018-11-09 09:04:25 +0100

    change static/media urls

--- src/etools_datamart/config/settings.py
+++ src/etools_datamart/config/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/dm-media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -127,7 +127,7 @@ MEDIA_URL = '/media/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/dm-static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
