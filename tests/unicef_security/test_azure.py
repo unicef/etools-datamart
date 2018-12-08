8b3657e5b60187f037b410ec2a68bc0532e1589f
sax <s.apostolico@gmail.com>
2018-12-08 17:40:17 +0100

    skip Azure tests in CircleCI

--- tests/unicef_security/test_azure.py
+++ tests/unicef_security/test_azure.py
@@ -1,14 +1,18 @@
+import os
 from pathlib import Path
 
+import pytest
 import vcr
 from unicef_security.graph import Synchronizer
 
 
+@pytest.mark.skipIf("CI" in os.environ and os.environ["CI"] == "true", "Skipping this test on CI.")
 def test_token():
     s = Synchronizer()
     assert s.access_token
 
 
+@pytest.mark.skipIf("CI" in os.environ and os.environ["CI"] == "true", "Skipping this test on CI.")
 @vcr.use_cassette(str(Path(__file__).parent / 'vcr_cassettes/test_user_data.yml'))
 def test_user_data():
     s = Synchronizer()
