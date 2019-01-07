fa4b4c37219b8af81ca8b4ec8882bd1b20226392
sax <s.apostolico@gmail.com>
2019-01-07 21:36:17 +0100

    silence 'UserWarning: The psycopg2...'

--- src/etools_datamart/__init__.py
+++ src/etools_datamart/__init__.py
@@ -1,3 +1,8 @@
+import warnings
+
 NAME = 'etools-datamart'
 VERSION = __version__ = '1.9a8'
 __author__ = ''
+
+# UserWarning: The psycopg2 wheel package will be renamed from release 2.8;
+warnings.simplefilter("ignore", UserWarning, 144)
