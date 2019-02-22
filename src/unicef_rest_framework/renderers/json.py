3c77ab2314febf343abf79e2541b8ce0014a8c25
sax <s.apostolico@gmail.com>
2019-02-22 21:34:07 +0100

    Bump version: 1.17.0a → 1.18.0a

---
+++ src/unicef_rest_framework/renderers/json.py
@@ -0,0 +1,22 @@
+import logging
+
+from crashlog.middleware import process_exception
+from rest_framework_csv import renderers as r
+
+logger = logging.getLogger(__name__)
+
+
+class CSVRenderer(r.CSVRenderer):
+
+    def render(self, data, media_type=None, renderer_context=None, writer_opts=None):
+        response = renderer_context['response']
+        if response.status_code != 200:
+            return ''
+        try:
+            if data and 'results' in data:
+                data = dict(data)['results']
+            return super().render(data, media_type, renderer_context, writer_opts)
+        except Exception as e:
+            process_exception(e)
+            logger.exception(e)
+            raise Exception('Error processing request')
