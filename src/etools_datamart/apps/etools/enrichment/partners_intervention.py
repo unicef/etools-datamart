7b19865ca7ad0dbcd8d15e60522a54767fb6f31a
saxix <s.apostolico@gmail.com>
2019-06-20 09:55:38 +0200

    new Intervention  enrichments

--- src/etools_datamart/apps/etools/enrichment/partners_intervention.py
+++ src/etools_datamart/apps/etools/enrichment/partners_intervention.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 from etools_datamart.apps.etools.enrichment.utils import create_alias
-from etools_datamart.apps.etools.models import (LocationsLocation, PartnersIntervention,
+from etools_datamart.apps.etools.models import (AuthUser, LocationsLocation, PartnersIntervention,
                                                 PartnersInterventionFlatLocations, PartnersInterventionOffices,
-                                                PartnersInterventionPartnerFocalPoints, PartnersInterventionSections,
-                                                PartnersPartnerstaffmember, ReportsSector, UsersOffice,)
+                                                PartnersInterventionPartnerFocalPoints, PartnersInterventionresultlink,
+                                                PartnersInterventionSections, PartnersInterventionUnicefFocalPoints,
+                                                PartnersPartnerstaffmember, ReportsResult, ReportsSector, UsersOffice,)
 
 PartnersIntervention.total_unicef_cash = property(
     lambda self: self.planned_budget.unicef_cash_local if hasattr(self, 'planned_budget') else 0)
@@ -54,9 +55,13 @@ models.ManyToManyField(LocationsLocation,
                        through=PartnersInterventionFlatLocations,
                        ).contribute_to_class(PartnersIntervention, 'flat_locations')
 
+models.ManyToManyField(AuthUser,
+                       through=PartnersInterventionUnicefFocalPoints,
+                       ).contribute_to_class(PartnersIntervention, 'unicef_focal_points')
+
 models.ManyToManyField(PartnersPartnerstaffmember,
                        through=PartnersInterventionPartnerFocalPoints,
-                       ).contribute_to_class(PartnersIntervention, 'unicef_focal_points')
+                       ).contribute_to_class(PartnersIntervention, 'partner_focal_points')
 
 models.ManyToManyField(UsersOffice,
                        through=PartnersInterventionOffices,
@@ -66,7 +71,23 @@ models.ManyToManyField(ReportsSector,
                        through=PartnersInterventionSections,
                        ).contribute_to_class(PartnersIntervention, 'sections')
 
+models.ManyToManyField(ReportsResult,
+                       through=PartnersInterventionresultlink,
+                       ).contribute_to_class(PartnersIntervention, 'result_links')
+
+# models.ManyToManyField(PartnersInterventionattachment,
+#                        through=PartnersInterventionAttachment,
+#                        ).contribute_to_class(PartnersIntervention, 'attachments')
+
 aliases = (['partnersintervention_partners_interventionbudget_intervention_id',
-            'planned_budget'], ['partnersintervention_funds_fundsreservationheader_intervention_id',
-                                'frs'])
+            'planned_budget'],
+           ['partnersintervention_funds_fundsreservationheader_intervention_id',
+            'frs'],
+           ['partnersintervention_partners_interventionattachment_intervention_id',
+            'attachments'],
+
+           # ['partnersintervention_partners_interventionplannedvisits_intervention_id',
+           #  'planned_visits'],
+
+           )
 create_alias(PartnersIntervention, aliases)
