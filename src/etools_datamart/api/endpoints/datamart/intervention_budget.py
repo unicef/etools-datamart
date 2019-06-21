67ec5febb91933285a750cbba74f437fe0eba04a
saxix <s.apostolico@gmail.com>
2019-06-21 13:36:29 +0200

    intervention budget endpoint

--- src/etools_datamart/api/endpoints/datamart/intervention_budget.py
+++ src/etools_datamart/api/endpoints/datamart/intervention_budget.py
@@ -8,8 +8,6 @@ from etools_datamart.api.endpoints.datamart.serializers import DataMartSerialize
 from etools_datamart.apps.data import models
 from etools_datamart.apps.etools.models import PartnersIntervention
 
-from .. import common
-
 
 class InterventionFilterForm(forms.Form):
     status__in = Select2MultipleChoiceField(label='Status',
@@ -32,6 +30,17 @@ class InterventionFilterForm(forms.Form):
         if 'status__in' in filters:
             filters.setlist('status__in', data['status__in'].split(','))
         super().__init__(filters, files, auto_id, prefix, initial, *args, **kwargs)
+
+
+class InterventionSerializerBudget(DataMartSerializer):
+    class Meta(DataMartSerializer.Meta):
+        model = models.InterventionBudget
+        exclude = None
+        fields = ('number', 'title', 'status', 'start_date', 'end_date',
+                  'partner_contribution', 'unicef_cash', 'in_kind_amount',
+                  'partner_contribution_local', 'unicef_cash_local', 'in_kind_amount_local',
+                  'total', 'total_local', 'currency',
+                  )
 
 
 class InterventionSerializerV2(DataMartSerializer):
@@ -98,46 +107,3 @@ class InterventionSerializerV2(DataMartSerializer):
                   'pd_ssfa_last_modify_date',
                   'pd_ssfa_url',
                   )
-
-
-class InterventionSerializerFull(DataMartSerializer):
-    class Meta(DataMartSerializer.Meta):
-        model = models.Intervention
-        exclude = ('seen',)
-
-
-class InterventionSerializer(InterventionSerializerFull):
-    class Meta(DataMartSerializer.Meta):
-        model = models.Intervention
-        exclude = ('metadata', 'partner_contribution', 'unicef_cash', 'in_kind_amount',
-                   'partner_contribution_local', 'unicef_cash_local', 'in_kind_amount_local',
-                   'total', 'total_local', 'currency', 'offices', 'locations')
-
-
-class InterventionByLocationSerializer(InterventionSerializerFull):
-    class Meta(DataMartSerializer.Meta):
-        model = models.InterventionByLocation
-        exclude = ('metadata', 'partner_contribution', 'unicef_cash', 'in_kind_amount',
-                   'partner_contribution_local', 'unicef_cash_local', 'in_kind_amount_local',
-                   'total', 'total_local', 'currency',)
-
-
-class InterventionViewSet(common.DataMartViewSet):
-    querystringfilter_form_base_class = InterventionFilterForm
-
-    serializer_class = InterventionSerializer
-    queryset = models.Intervention.objects.all()
-    filter_fields = ('status', 'last_modify_date', 'document_type',
-                     'start_date', 'submission_date',)
-    serializers_fieldsets = {'std': None,
-                             'full': InterventionSerializerFull,
-                             'v2': InterventionSerializerV2,
-                             'short': ["title", "number", "country_name", "start_date"]}
-
-    def get_querystringfilter_form(self, request, filter):
-        return InterventionFilterForm(request.GET, filter.form_prefix)
-
-
-class InterventionByLocationViewSet(InterventionViewSet):
-    serializer_class = InterventionByLocationSerializer
-    queryset = models.InterventionByLocation.objects.all()
