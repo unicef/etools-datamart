10edc4e10544082a79b12da2eb8d8b4ed291c6b2
saxix <s.apostolico@gmail.com>
2019-08-04 08:42:15 +0200

    updates requirements

--- src/etools_datamart/libs/time.py
+++ src/etools_datamart/libs/time.py
@@ -1,5 +1,6 @@
 def strfelapsed(seconds):
     # seconds = 9.888888888888886 * 60 * 60
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    return "%02d:%02d:%02d" % (hours, minutes, seconds)
+    if seconds:
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        return "%02d:%02d:%02d" % (hours, minutes, seconds)
