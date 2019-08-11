8c183e7b75f59b1c37f763d73548dd98c838d789
saxix <s.apostolico@gmail.com>
2019-08-11 08:35:41 +0200

    python 3.7

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,6 +1,4 @@
-import collections
 import json
-import pickle
 from pathlib import Path
 
 import pytest
@@ -37,21 +35,52 @@ def get_urls(data_file):
     return json.loads(data_file.read_text())
 
 
-@pytest.mark.django_db
-def test_urls():
-    contract_file = Path(__file__).parent / 'test_urls.json'
-    contract = get_urls(contract_file)
-    current = list_urls(urlpatterns)
-    # check all previous urls are preserved
-    for url in contract:
-        if url not in current:
-            pytest.fail("API REMOVED: '%s' endpoint has been removed" % url)
+def pytest_generate_tests(metafunc):
+    if 'current_url' in metafunc.fixturenames:
+        current = list_urls(urlpatterns)
+        metafunc.parametrize("current_url", current)
+    elif 'contract_url' in metafunc.fixturenames:
+        contract = get_urls(CONTRACT_FILE)
+        metafunc.parametrize("contract_url", contract)
 
-    # check for any added urls
-    for url in current:
-        if url not in contract:
-            pytest.fail("API ADDED: '%s' is a new url and contract file '%s' must be recreated" % (url, contract_file))
 
-    # for url in list_urls(urlpatterns):
-    #     assert url in contract
-    #
+CONTRACT_FILE = Path(__file__).parent / 'test_urls.json'
+
+
+@pytest.fixture(scope='module')
+def contract():
+    return get_urls(CONTRACT_FILE)
+
+
+@pytest.fixture(scope='module')
+def current():
+    return list_urls(urlpatterns)
+
+
+def test_addedd(contract, current_url):
+    if current_url not in contract:
+        pytest.fail("API ADDED: '%s' is a new url and contract file '%s' must be recreated" % (current_url,
+                                                                                               CONTRACT_FILE))
+
+
+def test_missing(current, contract_url):
+    if contract_url not in current:
+        pytest.fail("API REMOVED: '%s' endpoint has been removed" % contract_url)
+
+# def test_urls():
+#     contract_file = Path(__file__).parent / 'test_urls.json'
+#     contract = get_urls(contract_file)
+#     current = list_urls(urlpatterns)
+#     # check all previous urls are preserved
+#     for url in contract:
+#         if url not in current:
+#             pytest.fail("API REMOVED: '%s' endpoint has been removed" % url)
+#
+#     # check for any added urls
+#     for url in current:
+#         if url not in contract:
+#             pytest.fail("API ADDED: '%s' is a new url and contract file '%s' must be recreated" % (url, contract_file))
+#
+#     # for url in list_urls(urlpatterns):
+#     #     assert url in contract
+#     #
