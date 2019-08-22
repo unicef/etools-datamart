be5465cfe79cd873bb3fa872374e89b8cfcc1ae0
saxix <s.apostolico@gmail.com>
2019-08-22 16:46:30 +0200

    fixes Internet Explorer Content-Disposition issue

--- src/unicef_rest_framework/renderers/csv.py
+++ src/unicef_rest_framework/renderers/csv.py
@@ -3,13 +3,16 @@ import logging
 from crashlog.middleware import process_exception
 from rest_framework_csv import renderers as r
 
+from unicef_rest_framework.renderers.mixin import ContentDispositionMixin
+
 logger = logging.getLogger(__name__)
 
 
-class CSVRenderer(r.CSVRenderer):
+class CSVRenderer(ContentDispositionMixin, r.CSVRenderer):
 
     def render(self, data, media_type=None, renderer_context=None, writer_opts=None):
         response = renderer_context['response']
+        self.process_response(renderer_context)
         if response.status_code != 200:
             return ''
         try:
--- src/unicef_rest_framework/renderers/xls.py
+++ src/unicef_rest_framework/renderers/xls.py
@@ -3,21 +3,21 @@ import logging
 from crashlog.middleware import process_exception
 from drf_renderer_xlsx.renderers import XLSXRenderer as _XLSXRenderer
 
+from unicef_rest_framework.renderers.mixin import ContentDispositionMixin
+
 logger = logging.getLogger(__name__)
 
 
-class XLSXRenderer(_XLSXRenderer):
+class XLSXRenderer(ContentDispositionMixin, _XLSXRenderer):
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
         response = renderer_context['response']
+        self.process_response(renderer_context)
         if response.status_code != 200:
             return ''
         try:
-            # if data and 'results' in data:
-            #     data = data['results']
-            # if not data:
-            #     return ''
             return super().render(data, accepted_media_type, renderer_context)
+
         except Exception as e:
             process_exception(e)
             logger.exception(e)
