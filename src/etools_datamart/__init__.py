5aae87cc50f4b465f2c199689f2cdd869f309ff8
saxix <s.apostolico@gmail.com>
2019-08-31 19:01:27 +0200

    bump version

--- src/etools_datamart/__init__.py
+++ src/etools_datamart/__init__.py
@@ -1,7 +1,7 @@
 import warnings
 
 NAME = 'etools-datamart'
-VERSION = __version__ = '2.0.0rc22'
+VERSION = __version__ = '2.0'
 __author__ = ''
 
 # UserWarning: The psycopg2 wheel package will be renamed from release 2.8;
