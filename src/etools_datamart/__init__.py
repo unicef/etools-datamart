9e63b0437434822b11a159d198955c3629522c47
saxix <s.apostolico@gmail.com>
2019-09-04 14:36:20 +0200

    add some db indexes

--- src/etools_datamart/__init__.py
+++ src/etools_datamart/__init__.py
@@ -1,7 +1,7 @@
 import warnings
 
 NAME = 'etools-datamart'
-VERSION = __version__ = '2.0.1'
+VERSION = __version__ = '2.1.0a'
 __author__ = ''
 
 # UserWarning: The psycopg2 wheel package will be renamed from release 2.8;
