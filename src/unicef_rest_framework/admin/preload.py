714e0b84054e16949857c02757f863bf5b2a186a
saxix <s.apostolico@gmail.com>
2019-09-08 13:36:47 +0200

    format Preload response size

--- src/unicef_rest_framework/admin/preload.py
+++ src/unicef_rest_framework/admin/preload.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from admin_extra_urls.extras import action, ExtraUrlMixin
+
+from unicef_rest_framework.utils import humanize_size
 
 from etools_datamart.libs.admin_filters import SizeFilter, StatusFilter
 
@@ -22,16 +25,21 @@ def queue(modeladmin, request, queryset):
 
 
 class PreloadAdmin(ExtraUrlMixin, admin.ModelAdmin):
-    list_display = ('url', 'as_user', 'enabled', 'last_run', 'status_code', 'response_length', 'response_ms')
+    list_display = ('url', 'as_user', 'enabled', 'last_run', 'status_code', 'size', 'response_ms')
     date_hierarchy = 'last_run'
     search_fields = ('url',)
     list_filter = (StatusFilter, 'enabled', SizeFilter)
     actions = [queue, ]
+
     # form = PreloadForm
     # readonly_fields = ('params',)
     # formfield_overrides = {
     #     JSONField: {'widget': JSONEditor},
     # }
+
+    def size(self, obj):
+        if obj.response_length:
+            return mark_safe("<nobr>{0}</nobr>".format(humanize_size(obj.response_length)))
 
     @action()
     def queue(self, request, id):
