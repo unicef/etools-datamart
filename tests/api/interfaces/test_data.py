77a52ff86c80ccdb4b1e4d555fdae7bb4b236468
saxix <s.apostolico@gmail.com>
2019-09-16 21:19:00 +0200

    updates tests and deps

--- tests/api/interfaces/test_data.py
+++ tests/api/interfaces/test_data.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
-from functools import wraps
 
-import pytest
 from drf_api_checker.pytest import contract, frozenfixture
-from drf_api_checker.recorder import BASE_DATADIR, Recorder
+from drf_api_checker.recorder import Recorder
 from rest_framework.test import APIClient
 from test_utilities.factories import factories_registry, UserFactory
 
@@ -20,34 +18,34 @@ class MyRecorder(Recorder):
         return client
 
 
-def frozenfixture2(use_request=False):
-    def deco(func):
-        from drf_api_checker.utils import load_fixtures, dump_fixtures
-        from drf_api_checker.fs import mktree
-
-        @wraps(func)
-        def _inner(*args, **kwargs):
-            parts = [os.path.dirname(func.__code__.co_filename),
-                     BASE_DATADIR,
-                     func.__module__,
-                     func.__name__, ]
-            if 'request' in kwargs:
-                request = kwargs['request']
-                viewset = request.getfixturevalue('viewset')
-                parts.append(viewset.__name__)
-
-            destination = os.path.join(*parts
-                                       ) + '.fixture.json'
-            if os.path.exists(destination) and not os.environ.get('API_CHECKER_RESET'):
-                return load_fixtures(destination)[func.__name__]
-            mktree(os.path.dirname(destination))
-            data = func(*args, **kwargs)
-            dump_fixtures({func.__name__: data}, destination)
-            return data
-
-        return pytest.fixture(_inner)
-
-    return deco
+# def frozenfixture2(use_request=False):
+#     def deco(func):
+#         from drf_api_checker.utils import load_fixtures, dump_fixtures
+#         from drf_api_checker.fs import mktree
+#
+#         @wraps(func)
+#         def _inner(*args, **kwargs):
+#             parts = [os.path.dirname(func.__code__.co_filename),
+#                      BASE_DATADIR,
+#                      func.__module__,
+#                      func.__name__, ]
+#             if 'request' in kwargs:
+#                 request = kwargs['request']
+#                 viewset = request.getfixturevalue('viewset')
+#                 parts.append(viewset.__name__)
+#
+#             destination = os.path.join(*parts
+#                                        ) + '.fixture.json'
+#             if os.path.exists(destination) and not os.environ.get('API_CHECKER_RESET'):
+#                 return load_fixtures(destination)[func.__name__]
+#             mktree(os.path.dirname(destination))
+#             data = func(*args, **kwargs)
+#             dump_fixtures({func.__name__: data}, destination)
+#             return data
+#
+#         return pytest.fixture(_inner)
+#
+#     return deco
 
 
 def pytest_generate_tests(metafunc, *args):
@@ -67,7 +65,17 @@ def pytest_generate_tests(metafunc, *args):
         metafunc.parametrize("viewset,serializer", params, ids=ids)
 
 
-@frozenfixture
+def url(request):
+    return ""
+
+
+def aaaa(seed, request):
+    viewset = request.getfixturevalue('viewset')
+    url = viewset.get_service().endpoint.strip('.').replace('/', '_')
+    return os.path.join(seed, url) + '.fixture.json'
+
+
+@frozenfixture(fixture_name=aaaa)
 def data(db, request):
     # TIPS: database access is forbidden in pytest_generate_tests
     viewset = request.getfixturevalue('viewset')
@@ -79,12 +87,12 @@ def data(db, request):
 
 
 @contract(recorder_class=MyRecorder)
-def test_list(viewset, serializer, data):
+def test_list(request, viewset, serializer, data):
     url = f"{viewset.get_service().endpoint}"
     return [url, {'-serializer': serializer}]
 
 
-@frozenfixture2()
+@frozenfixture(fixture_name=aaaa)
 def record(db, request):
     # TIPS: database access is forbidden in pytest_generate_tests
     viewset = request.getfixturevalue('viewset')
@@ -93,6 +101,6 @@ def record(db, request):
 
 
 @contract(recorder_class=MyRecorder)
-def test_record(viewset, serializer, record):
+def test_record(request, viewset, serializer, record):
     url = f"{viewset.get_service().endpoint}{record.pk}/"
     return url
