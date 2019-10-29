476077461769b9c3d2a4367e7ab386a01d4053e2
saxix <s.apostolico@gmail.com>
2019-10-29 16:46:46 +0100

    add check to pevent missing filter declaration

--- src/etools_datamart/apps/core/checks.py
+++ src/etools_datamart/apps/core/checks.py
@@ -1,9 +1,26 @@
+from unittest.mock import Mock, MagicMock
+
 from django.core import checks
 
 # from django.core.checks import Error
+from django.core.checks import Error
+
+from etools_datamart.api.urls import router
 
 
 @checks.register()
 def check_loader(app_configs, **kwargs):
     errors = []
+    for prefix, viewset, basename in router.registry:
+        if hasattr(viewset, 'querystringfilter_form_base_class'):
+            view = viewset()
+            form = view.get_querystringfilter_form(MagicMock(), MagicMock())
+            a = set(form.fields.keys())
+            b = set(viewset.filter_fields)
+            not_enabled = a - b
+            if not_enabled:
+                errors.append(Error('%s %s enabled but not showed in form' % (viewset.__name__, not_enabled)))
+            # missed_in_form = b - a
+            # if missed_in_form:
+            #     errors.append(Error('%s form is missing %s' % (viewset.__name__, missed_in_form)))
     return errors
