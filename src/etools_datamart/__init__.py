87cf62e0aca6f80ec8b10b8ae539c5f1c6f80311
sax <s.apostolico@gmail.com>
2019-12-15 20:59:45 +0100

    Bump version: 2.10.0a7 → 2.10.0a8

--- src/etools_datamart/__init__.py
+++ src/etools_datamart/__init__.py
@@ -1,8 +1,8 @@
 import warnings
 
 NAME = 'etools-datamart'
-VERSION = __version__ = '2.10.0a7'
+VERSION = __version__ = '2.10.0a8'
 __author__ = ''
 
-# UserWarning: The psycopg2 wheel package will be renamed from release 2.10.0a7;
+# UserWarning: The psycopg2 wheel package will be renamed from release 2.10.0a8;
 warnings.simplefilter("ignore", UserWarning, 144)
