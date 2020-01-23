3c6bcd1aeb6e00500b38620d8987370417a87554
sax <s.apostolico@gmail.com>
2020-01-23 07:12:56 +0100

    updates CHANGES

--- src/etools_datamart/__init__.py
+++ src/etools_datamart/__init__.py
@@ -1,8 +1,8 @@
 import warnings
 
 NAME = 'etools-datamart'
-VERSION = __version__ = '2.10.3rc'
+VERSION = __version__ = '2.10.3'
 __author__ = ''
 
-# UserWarning: The psycopg2 wheel package will be renamed from release 2.10.3rc;
+# UserWarning: The psycopg2 wheel package will be renamed from release 2.10.3;
 warnings.simplefilter("ignore", UserWarning, 144)
