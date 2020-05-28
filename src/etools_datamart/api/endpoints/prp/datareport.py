0b5f0e2d8aa5c288251c1175807235610323312a
Greg Reinbach <greg@reinbach.com>
2020-05-28 09:31:54 -0400

    Add filtering on country_name to prp datareport

--- src/etools_datamart/api/endpoints/prp/datareport.py
+++ src/etools_datamart/api/endpoints/prp/datareport.py
@@ -7,10 +7,24 @@ from etools_datamart.api.endpoints.common import DataMartViewSet
 from etools_datamart.api.endpoints.datamart.serializers import DataMartSerializer
 from etools_datamart.api.filtering import DatamartQueryStringFilterBackend
 from etools_datamart.apps.mart.prp import models
+from unicef_rest_framework.forms import Select2MultipleChoiceField
 
 
 class DataReportFilterForm(forms.Form):
-    pass
+    country_name__in = Select2MultipleChoiceField(
+        label='Country',
+        choices=[],
+        required=False,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["country_name__in"].choices = [
+            (c.country_name, c.country_name)
+            for c in models.DataReport.objects.distinct(
+                    "country_name"
+            ).order_by("country_name")
+        ]
 
 
 class DataReportSerializer(DataMartSerializer):
@@ -23,13 +37,14 @@ class DataReportSerializer(DataMartSerializer):
 class DataReportViewSet(DataMartViewSet):
     serializer_class = DataReportSerializer
     queryset = models.DataReport.objects.all()
-    # filter_fields = ('created', 'date_of_completion', 'due_date', 'high_priority')
+    filter_fields = ('country_name',)
     serializers_fieldsets = {'std': DataReportSerializer, }
     querystringfilter_form_base_class = DataReportFilterForm
-    filter_backends = [DatamartQueryStringFilterBackend,
-                       OrderingFilter,
-                       DynamicSerializerFilter,
-                       ]
+    filter_backends = [
+        DatamartQueryStringFilterBackend,
+        OrderingFilter,
+        DynamicSerializerFilter,
+    ]
 
     def get_serializer(self, *args, **kwargs):
         return super().get_serializer(*args, **kwargs)
