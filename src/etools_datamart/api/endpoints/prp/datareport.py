480d3fd350ebb810090127da7e9bc4d7f3a9a99a
Greg Reinbach <greg@reinbach.com>
2020-05-28 13:19:00 -0400

    Add datareport filtering for report_status, report_type, and section

--- src/etools_datamart/api/endpoints/prp/datareport.py
+++ src/etools_datamart/api/endpoints/prp/datareport.py
@@ -1,13 +1,14 @@
 from django import forms
 
 from unicef_rest_framework.ds import DynamicSerializerFilter
+from unicef_rest_framework.forms import Select2MultipleChoiceField
 from unicef_rest_framework.ordering import OrderingFilter
 
 from etools_datamart.api.endpoints.common import DataMartViewSet
 from etools_datamart.api.endpoints.datamart.serializers import DataMartSerializer
 from etools_datamart.api.filtering import DatamartQueryStringFilterBackend
 from etools_datamart.apps.mart.prp import models
-from unicef_rest_framework.forms import Select2MultipleChoiceField
+from etools_datamart.apps.sources.source_prp.enrichment.consts import PROGRESS_REPORT_STATUS, REPORTING_TYPES
 
 
 class DataReportFilterForm(forms.Form):
@@ -16,15 +17,43 @@ class DataReportFilterForm(forms.Form):
         choices=[],
         required=False,
     )
+    report_status__in = Select2MultipleChoiceField(
+        label='Report Status',
+        choices=PROGRESS_REPORT_STATUS,
+        required=False,
+    )
+    report_type__in = Select2MultipleChoiceField(
+        label='Type of Report',
+        choices=REPORTING_TYPES,
+        required=False,
+    )
+    section__icontains = Select2MultipleChoiceField(
+        label='Section',
+        choices=[],
+        required=False,
+    )
+
+    def get_section_choices(self):
+        sections = []
+        for record in models.DataReport.objects.all():
+            sections += [s.strip() for s in record.section.split(",")]
+        return [(s, s) for s in set(sections)]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["country_name__in"].choices = [
-            (c.country_name, c.country_name)
-            for c in models.DataReport.objects.distinct(
-                    "country_name"
-            ).order_by("country_name")
-        ]
+        try:
+            self.fields["country_name__in"].choices = [
+                (c.country_name, c.country_name)
+                for c in models.DataReport.objects.distinct(
+                        "country_name"
+                ).order_by("country_name")
+            ]
+            self.fields["section__in"].choices = self.get_section_choices()
+        except:
+            # Ignore this exception, as it is used to catch exception during
+            # changes to the DataReport model, or running migrations
+            # Worse case is choice for filtering is blank
+            pass
 
 
 class DataReportSerializer(DataMartSerializer):
@@ -37,7 +66,7 @@ class DataReportSerializer(DataMartSerializer):
 class DataReportViewSet(DataMartViewSet):
     serializer_class = DataReportSerializer
     queryset = models.DataReport.objects.all()
-    filter_fields = ('country_name',)
+    filter_fields = ('country_name', 'report_status', 'report_type', 'section')
     serializers_fieldsets = {'std': DataReportSerializer, }
     querystringfilter_form_base_class = DataReportFilterForm
     filter_backends = [
