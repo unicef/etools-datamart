b739f90aed8ac61df99c6b88138809a9b6828c37
Greg Reinbach <greg@reinbach.com>
2020-06-04 13:50:55 -0400

    Update data migration sequence

--- src/etools_datamart/apps/mart/data/migrations/0119_engagement_auditor.py
+++ src/etools_datamart/apps/mart/data/migrations/0119_engagement_auditor.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data', '0117_auto_20200421_1544'),
+        ('data', '0118_fundsreservation_pd_reference_number'),
     ]
 
     operations = [
