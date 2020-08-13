9a3e656968412fcdf6f0667b94df3756dce845e4
Greg Reinbach <greg@reinbach.com>
2020-08-13 13:03:26 -0400

    Fix migration reference

--- src/etools_datamart/apps/mart/data/migrations/0124_fmontrack_fmquestion.py
+++ src/etools_datamart/apps/mart/data/migrations/0124_fmontrack_fmquestion.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data', '0123_auto_0200811_2129'),
+        ('data', '0123_auto_20200811_2129'),
     ]
 
     operations = [
