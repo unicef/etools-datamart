fd495f94520e66d754265c2885a3a6fa54b322b7
Greg Reinbach <greg@reinbach.com>
2020-08-13 16:27:51 -0400

    Fix migration sequence

--- src/etools_datamart/apps/mart/data/migrations/0125_auto_20200811_2129.py
+++ src/etools_datamart/apps/mart/data/migrations/0125_auto_20200811_2129.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data', '0123_interventionbylocation_p_code'),
+        ('data', '0124_fmontract_fmquestion'),
     ]
 
     operations = [
