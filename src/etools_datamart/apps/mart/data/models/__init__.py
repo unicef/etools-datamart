5431cb83a8ae8570e3d547bdfa250ca99e8a1475
Greg Reinbach <greg@reinbach.com>
2020-12-17 13:26:15 -0500

    Add audit special model/loader

--- src/etools_datamart/apps/mart/data/models/__init__.py
+++ src/etools_datamart/apps/mart/data/models/__init__.py
@@ -4,6 +4,7 @@ from .audit_audit import Audit  # noqa
 from .audit_engagement import Engagement  # noqa
 from .audit_micro_assessment import MicroAssessment
 from .audit_result import AuditResult  # noqa
+from .audit_special import AuditSpecial  # noqa
 from .audit_spotcheck import SpotCheck  # noqa
 from .fam import FAMIndicator  # noqa
 from .fm_questions import FMOntrack, FMQuestion  # noqa
