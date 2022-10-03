c1ee51d74ba7d783fe220cab1422bc64db38b3ab
Domenico DiNicola <dom.dinicola@gmail.com>
2022-10-03 12:28:13 -0500

    3.46.1

--- src/etools_datamart/apps/mart/data/fields.py
+++ src/etools_datamart/apps/mart/data/fields.py
@@ -6,7 +6,7 @@ class SafeDecimal(Decimal):
     def __new__(cls, value="0", context=None):
         if value is None:
             return None
-        if isinstance(value, int):
+        if isinstance(value, (int, float)):
             value = str(value)
         if isinstance(value, str) and ',' in value:
             value = value.replace(',', '.')
