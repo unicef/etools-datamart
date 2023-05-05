e22a5925dc86509287bcab19afaa3199a067d414
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2023-05-05 11:54:45 +0300

    Added admin filter/search fields for DataReport, IndicatorReport, IndicatorByLocationAdmin

--- src/etools_datamart/apps/mart/prp/admin.py
+++ src/etools_datamart/apps/mart/prp/admin.py
@@ -19,6 +19,8 @@ class IndicatorByLocationAdmin(TruncateTableMixin, ModelAdmin):
         "reference_number",
         "title_of_indicator",
     )
+    list_filter = ("country",)
+    search_fields = list_display
 
 
 @register(models.DataReport)
@@ -29,13 +31,31 @@ class DataReportAdmin(TruncateTableMixin, ModelAdmin):
         "partner_name",
         "cp_output",
         "intervention_reference_number",
-        "pd_result",
+        "report_status",
+        "submitted_by",
     )
-    list_filter = ("country_name", "partner_name", "cp_output", "intervention_reference_number")
+    list_filter = ("country_name", "partner_name", "cp_output", "report_status")
+    search_fields = (
+        "current_location",
+        "country_name",
+        "partner_name",
+        "cp_output",
+        "intervention_reference_number",
+        "report_status",
+        "submitted_by",
+        "section",
+    )
 
 
 @register(models.IndicatorReport)
 class IndicatorReportAdmin(TruncateTableMixin, ModelAdmin):
-    list_display = ("business_area", "pd_output_progress_status", "performance_indicator")
-    list_filter = ("business_area", "pd_output_progress_status")
+    list_display = (
+        "business_area",
+        "pd_output_progress_status",
+        "report_type",
+        "time_period_start",
+        "time_period_end",
+        "performance_indicator",
+    )
+    list_filter = ("business_area", "pd_output_progress_status", "report_type")
     search_fields = ("performance_indicator",)
