051332a468eb03454857cb2e3deba978882fd1e5
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2024-02-08 13:15:36 +0200

    isort

--- src/etools_datamart/api/endpoints/datamart/fm_questions.py
+++ src/etools_datamart/api/endpoints/datamart/fm_questions.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
 from django import forms
+
 from constance import config
+
+from unicef_rest_framework.forms import Select2ChoiceField
 
 from etools_datamart.api.endpoints.common import DataMartViewSet
 from etools_datamart.api.endpoints.datamart.serializers import DataMartSerializer
 from etools_datamart.apps.mart.data import models
-from unicef_rest_framework.forms import Select2ChoiceField
 
 
 class FMQuestionSerializer(DataMartSerializer):
