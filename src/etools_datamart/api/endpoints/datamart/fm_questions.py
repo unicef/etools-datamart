7b0eabeec89b51f843c7faafa510d611768fcacc
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2024-02-08 12:45:36 +0200

    [ch36256] Export 555 Fm Questions: added year filter for /fm-questions/

--- src/etools_datamart/api/endpoints/datamart/fm_questions.py
+++ src/etools_datamart/api/endpoints/datamart/fm_questions.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+
+from django import forms
+from constance import config
+
 from etools_datamart.api.endpoints.common import DataMartViewSet
 from etools_datamart.api.endpoints.datamart.serializers import DataMartSerializer
 from etools_datamart.apps.mart.data import models
+from unicef_rest_framework.forms import Select2ChoiceField
 
 
 class FMQuestionSerializer(DataMartSerializer):
@@ -8,9 +14,23 @@ class FMQuestionSerializer(DataMartSerializer):
         model = models.FMQuestion
 
 
+class FMQuestionFilterForm(forms.Form):
+    year = Select2ChoiceField(
+        label="Monitoring Activity End Year",
+        choices=[(r, r) for r in range(datetime.now().year, config.START_YEAR_FILTER - 1, -1)],
+        required=False,
+    )
+
+
 class FMQuestionViewSet(DataMartViewSet):
     serializer_class = FMQuestionSerializer
     queryset = models.FMQuestion.objects.all()
+    filter_fields = ("year",)
+    querystringfilter_form_base_class = FMQuestionFilterForm
+
+    def drfqs_filter_year(self, filters, exclude, value, **payload):
+        filters["monitoring_activity_end_date__contains"] = value
+        return filters, exclude
 
 
 class FMOntrackSerializer(DataMartSerializer):
