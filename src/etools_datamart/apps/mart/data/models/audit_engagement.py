6b9da875e10144c57e40e9686febafe57dc383b0
Hakan <hakan.barisik@arobs.com>
2024-12-05 15:55:30 +0300

    document queries for engagement

--- src/etools_datamart/apps/mart/data/models/audit_engagement.py
+++ src/etools_datamart/apps/mart/data/models/audit_engagement.py
@@ -160,6 +160,356 @@ class EngagementMixin:
 
 
 class EngagementlLoader(EngagementMixin, EtoolsLoader):
+    """
+    -- Set country schema
+    SET search_path = public, ##COUNTRY##;
+
+    -- Count for paging;
+    SELECT COUNT(*) AS "__count" FROM "audit_engagement";
+
+    -- Audit
+
+    SELECT '##COUNTRY##' AS __schema,
+           "audit_microassessment"."engagement_ptr_id",
+           "audit_microassessment"."questionnaire_version",
+
+           "audit_engagement"."id",
+           "audit_engagement"."created",
+           "audit_engagement"."modified",
+           "audit_engagement"."status",
+           "audit_engagement"."partner_contacted_at",
+           "audit_engagement"."engagement_type",
+           "audit_engagement"."start_date",
+           "audit_engagement"."end_date",
+           "audit_engagement"."total_value",
+           "audit_engagement"."date_of_field_visit",
+           "audit_engagement"."date_of_draft_report_to_ip",
+           "audit_engagement"."date_of_comments_by_ip",
+           "audit_engagement"."date_of_draft_report_to_unicef",
+           "audit_engagement"."date_of_comments_by_unicef",
+           "audit_engagement"."date_of_report_submit",
+           "audit_engagement"."date_of_final_report",
+           "audit_engagement"."date_of_cancel",
+           "audit_engagement"."amount_refunded",
+           "audit_engagement"."additional_supporting_documentation_provided",
+           "audit_engagement"."justification_provided_and_accepted",
+           "audit_engagement"."write_off_required",
+           "audit_engagement"."cancel_comment",
+           "audit_engagement"."explanation_for_additional_information",
+           "audit_engagement"."partner_id", "audit_engagement"."joint_audit",
+           "audit_engagement"."agreement_id", "audit_engagement"."po_item_id",
+           "audit_engagement"."shared_ip_with", "audit_engagement"."exchange_rate",
+           "audit_engagement"."currency_of_report",
+           "audit_engagement"."reference_number",
+           "audit_engagement"."year_of_audit",
+
+           "partners_partnerorganization"."id",
+           "partners_partnerorganization"."description",
+           "partners_partnerorganization"."address",
+           "partners_partnerorganization"."email",
+           "partners_partnerorganization"."phone_number",
+           "partners_partnerorganization"."alternate_id",
+           "partners_partnerorganization"."alternate_name",
+           "partners_partnerorganization"."rating",
+           "partners_partnerorganization"."core_values_assessment_date",
+           "partners_partnerorganization"."vision_synced",
+           "partners_partnerorganization"."type_of_assessment",
+           "partners_partnerorganization"."last_assessment_date",
+           "partners_partnerorganization"."hidden",
+           "partners_partnerorganization"."deleted_flag",
+           "partners_partnerorganization"."total_ct_cp",
+           "partners_partnerorganization"."total_ct_cy",
+           "partners_partnerorganization"."blocked",
+           "partners_partnerorganization"."city",
+           "partners_partnerorganization"."country",
+           "partners_partnerorganization"."postal_code",
+           "partners_partnerorganization"."shared_with",
+           "partners_partnerorganization"."street_address",
+           "partners_partnerorganization"."hact_values",
+           "partners_partnerorganization"."created",
+           "partners_partnerorganization"."modified",
+           "partners_partnerorganization"."net_ct_cy",
+           "partners_partnerorganization"."reported_cy",
+           "partners_partnerorganization"."total_ct_ytd",
+           "partners_partnerorganization"."basis_for_risk_rating",
+           "partners_partnerorganization"."manually_blocked",
+           "partners_partnerorganization"."outstanding_dct_amount_6_to_9_months_usd",
+           "partners_partnerorganization"."outstanding_dct_amount_more_than_9_months_usd",
+           "partners_partnerorganization"."highest_risk_rating_name",
+           "partners_partnerorganization"."highest_risk_rating_type",
+           "partners_partnerorganization"."psea_assessment_date",
+           "partners_partnerorganization"."sea_risk_rating_name",
+           "partners_partnerorganization"."lead_office_id",
+           "partners_partnerorganization"."lead_section_id",
+           "partners_partnerorganization"."organization_id",
+
+           "purchase_order_purchaseorder"."id",
+           "purchase_order_purchaseorder"."created",
+           "purchase_order_purchaseorder"."modified",
+           "purchase_order_purchaseorder"."order_number",
+           "purchase_order_purchaseorder"."contract_start_date",
+           "purchase_order_purchaseorder"."contract_end_date",
+           "purchase_order_purchaseorder"."auditor_firm_id",
+           "purchase_order_auditorfirm"."id",
+           "purchase_order_auditorfirm"."created",
+           "purchase_order_auditorfirm"."modified",
+           "purchase_order_auditorfirm"."street_address",
+           "purchase_order_auditorfirm"."city",
+           "purchase_order_auditorfirm"."postal_code",
+           "purchase_order_auditorfirm"."country",
+           "purchase_order_auditorfirm"."email",
+           "purchase_order_auditorfirm"."phone_number",
+           "purchase_order_auditorfirm"."blocked",
+           "purchase_order_auditorfirm"."hidden",
+           "purchase_order_auditorfirm"."deleted_flag",
+           "purchase_order_auditorfirm"."vision_synced",
+           "purchase_order_auditorfirm"."unicef_users_allowed",
+           "purchase_order_auditorfirm"."organization_id",
+
+           "organizations_organization"."id",
+           "organizations_organization"."created",
+           "organizations_organization"."modified",
+           "organizations_organization"."name",
+           "organizations_organization"."vendor_number",
+           "organizations_organization"."organization_type",
+           "organizations_organization"."cso_type",
+           "organizations_organization"."short_name",
+           "organizations_organization"."other",
+           "organizations_organization"."parent_id",
+
+           "purchase_order_purchaseorderitem"."id",
+           "purchase_order_purchaseorderitem"."number",
+           "purchase_order_purchaseorderitem"."purchase_order_id"
+
+    FROM "audit_microassessment"
+         INNER JOIN "audit_engagement" ON ("audit_microassessment"."engagement_ptr_id" = "audit_engagement"."id")
+         INNER JOIN "partners_partnerorganization" ON ("audit_engagement"."partner_id" = "partners_partnerorganization"."id")
+         INNER JOIN "purchase_order_purchaseorder" ON ("audit_engagement"."agreement_id" = "purchase_order_purchaseorder"."id")
+         INNER JOIN "purchase_order_auditorfirm" ON ("purchase_order_purchaseorder"."auditor_firm_id" = "purchase_order_auditorfirm"."id")
+         INNER JOIN "organizations_organization" ON ("purchase_order_auditorfirm"."organization_id" = "organizations_organization"."id")
+         LEFT OUTER JOIN "purchase_order_purchaseorderitem" ON ("audit_engagement"."po_item_id" = "purchase_order_purchaseorderitem"."id")
+    ORDER BY "audit_engagement"."id" ASC
+    LIMIT ##PAGE_SIZE## OFFSET ##PAGE_OFFSET##;
+
+    -- Authorized officers for the page
+    SELECT ("audit_engagement_authorized_officers"."engagement_id") AS "_prefetch_related_val_engagement_id",
+           "auth_user"."id",
+           "auth_user"."password",
+           "auth_user"."last_login",
+           "auth_user"."is_superuser",
+           "auth_user"."username",
+           "auth_user"."first_name",
+           "auth_user"."last_name",
+           "auth_user"."email",
+           "auth_user"."is_staff",
+           "auth_user"."is_active",
+           "auth_user"."date_joined",
+           "auth_user"."middle_name",
+           "auth_user"."created",
+           "auth_user"."modified",
+           "auth_user"."preferences"
+    FROM "auth_user"
+         INNER JOIN "audit_engagement_authorized_officers" ON ("auth_user"."id" = "audit_engagement_authorized_officers"."user_id")
+    WHERE "audit_engagement_authorized_officers"."engagement_id" IN ( ##LIST OF ENGAGEMENT_IDs IN THE PAGE## );
+
+    -- Authorized staff members for the page
+    SELECT ("audit_engagement_staff_members"."engagement_id") AS "_prefetch_related_val_engagement_id",
+           "auth_user"."id",
+           "auth_user"."password",
+           "auth_user"."last_login",
+           "auth_user"."is_superuser",
+           "auth_user"."username",
+           "auth_user"."first_name",
+           "auth_user"."last_name",
+           "auth_user"."email",
+           "auth_user"."is_staff",
+           "auth_user"."is_active",
+           "auth_user"."date_joined",
+           "auth_user"."middle_name",
+           "auth_user"."created",
+           "auth_user"."modified",
+           "auth_user"."preferences"
+    FROM "auth_user"
+         INNER JOIN "audit_engagement_staff_members" ON ("auth_user"."id" = "audit_engagement_staff_members"."user_id")
+    WHERE "audit_engagement_staff_members"."engagement_id" IN IN ( ##LIST OF ENGAGEMENT_IDs IN THE PAGE## );
+
+
+    -- Audit Engagement Sections for the page
+    SELECT '##COUNTRY##' AS __schema,
+           "audit_engagement_sections"."id",
+           "audit_engagement_sections"."engagement_id",
+           "audit_engagement_sections"."section_id"
+    FROM "audit_engagement_sections"
+    WHERE "audit_engagement_sections"."engagement_id" IN ( ##LIST OF ENGAGEMENT_IDs IN THE PAGE## );
+
+
+    -- action_points_actionpoint for the page
+    SELECT '##COUNTRY##' AS __schema, "action_points_actionpoint"."id",
+           "action_points_actionpoint"."created",
+           "action_points_actionpoint"."modified",
+           "action_points_actionpoint"."status",
+           "action_points_actionpoint"."description",
+           "action_points_actionpoint"."due_date",
+           "action_points_actionpoint"."date_of_completion",
+           "action_points_actionpoint"."assigned_by_id",
+           "action_points_actionpoint"."assigned_to_id",
+           "action_points_actionpoint"."author_id",
+           "action_points_actionpoint"."cp_output_id",
+           "action_points_actionpoint"."engagement_id",
+           "action_points_actionpoint"."intervention_id",
+           "action_points_actionpoint"."location_id",
+           "action_points_actionpoint"."office_id",
+           "action_points_actionpoint"."partner_id",
+           "action_points_actionpoint"."section_id",
+           "action_points_actionpoint"."tpm_activity_id",
+           "action_points_actionpoint"."high_priority",
+           "action_points_actionpoint"."travel_activity_id",
+           "action_points_actionpoint"."category_id",
+           "action_points_actionpoint"."psea_assessment_id",
+           "action_points_actionpoint"."reference_number",
+           "action_points_actionpoint"."monitoring_activity_id"
+    FROM "action_points_actionpoint"
+    WHERE "action_points_actionpoint"."engagement_id" IN ( ##LIST OF ENGAGEMENT_IDs IN THE PAGE## );
+
+
+    -- audit_engagement_active_pd for the page
+    SELECT '##COUNTRY##' AS __schema,
+           "audit_engagement_active_pd"."id",
+           "audit_engagement_active_pd"."engagement_id",
+           "audit_engagement_active_pd"."intervention_id"
+    FROM "audit_engagement_active_pd"
+    WHERE "audit_engagement_active_pd"."engagement_id" IN ( ##LIST OF ENGAGEMENT_IDs IN THE PAGE## );
+
+
+    --
+    SELECT '##COUNTRY##' AS __schema, "audit_risk"."id",
+           "audit_risk"."value",
+           "audit_risk"."extra",
+           "audit_risk"."blueprint_id",
+           "audit_risk"."engagement_id"
+    FROM "audit_risk"
+         INNER JOIN "audit_riskblueprint" ON ("audit_risk"."blueprint_id" = "audit_riskblueprint"."id")
+         INNER JOIN "audit_riskcategory" ON ("audit_riskblueprint"."category_id" = "audit_riskcategory"."id")
+    WHERE ("audit_riskcategory"."header" = 'Key Internal Controls Weaknesses' AND "audit_risk"."engagement_id"
+            IN (##LIST OF ENGAGEMENT_IDs IN THE PAGE##))
+
+
+    --
+    SELECT "organizations_organization"."id",
+           "organizations_organization"."created",
+           "organizations_organization"."modified",
+           "organizations_organization"."name",
+           "organizations_organization"."vendor_number",
+           "organizations_organization"."organization_type",
+           "organizations_organization"."cso_type",
+           "organizations_organization"."short_name",
+           "organizations_organization"."other",
+           "organizations_organization"."parent_id"
+    FROM "organizations_organization" WHERE "organizations_organization"."id"
+         IN (##LIST OF "organizations_organization"."id" in tje main recordset IN THE PAGE ## );
+
+
+    --
+    SELECT '##COUNTRY##' AS __schema,
+           "audit_engagement_sections"."id",
+           "audit_engagement_sections"."engagement_id",
+           "audit_engagement_sections"."section_id"
+    FROM "audit_engagement_sections"
+    WHERE "audit_engagement_sections"."engagement_id" IN ( ##LIST OF ENGAGEMENT_IDs IN THE PAGE## );
+
+
+    --
+    SELECT "django_content_type"."id",
+           "django_content_type"."app_label",
+           "django_content_type"."model"
+    FROM "django_content_type"
+    WHERE ("django_content_type"."app_label" = 'audit' AND "django_content_type"."model" = 'engagement');
+
+
+    --
+    SELECT "unicef_attachments_attachment"."file"
+    FROM "unicef_attachments_attachment"
+    WHERE ("unicef_attachments_attachment"."code" = 'audit_engagement'
+           AND "unicef_attachments_attachment"."content_type_id" = 216
+           AND "unicef_attachments_attachment"."object_id" IN ( ##LIST OF ENGAGEMENT_IDs IN THE PAGE##  )
+
+    --
+    SELECT "unicef_attachments_attachment"."file"
+    FROM "unicef_attachments_attachment"
+    WHERE ("unicef_attachments_attachment"."code" = 'audit_report'
+           AND "unicef_attachments_attachment"."content_type_id" = 216
+           AND "unicef_attachments_attachment"."object_id"  IN ( ##LIST OF ENGAGEMENT_IDs IN THE PAGE##  )
+
+
+    --
+    SELECT '##COUNTRY##' AS __schema,
+           "partners_intervention"."id",
+           "partners_intervention"."created",
+           "partners_intervention"."modified",
+           "partners_intervention"."document_type",
+           "partners_intervention"."number",
+           "partners_intervention"."title",
+           "partners_intervention"."status",
+           "partners_intervention"."start",
+           "partners_intervention"."end",
+           "partners_intervention"."submission_date",
+           "partners_intervention"."submission_date_prc",
+           "partners_intervention"."review_date_prc",
+           "partners_intervention"."prc_review_document",
+           "partners_intervention"."signed_by_unicef_date",
+           "partners_intervention"."signed_by_partner_date",
+           "partners_intervention"."population_focus",
+           "partners_intervention"."agreement_id",
+           "partners_intervention"."unicef_signatory_id",
+           "partners_intervention"."signed_pd_document",
+           "partners_intervention"."country_programme_id",
+           "partners_intervention"."contingency_pd",
+           "partners_intervention"."metadata",
+           "partners_intervention"."in_amendment",
+           "partners_intervention"."reference_number_year",
+           "partners_intervention"."activation_letter",
+           "partners_intervention"."termination_doc",
+           "partners_intervention"."cfei_number",
+           "partners_intervention"."budget_owner_id",
+           "partners_intervention"."context",
+           "partners_intervention"."date_sent_to_partner",
+           "partners_intervention"."equity_narrative",
+           "partners_intervention"."equity_rating",
+           "partners_intervention"."gender_narrative",
+           "partners_intervention"."gender_rating",
+           "partners_intervention"."hq_support_cost",
+           "partners_intervention"."implementation_strategy",
+           "partners_intervention"."ip_program_contribution",
+           "partners_intervention"."partner_accepted",
+           "partners_intervention"."sustainability_narrative",
+           "partners_intervention"."sustainability_rating",
+           "partners_intervention"."unicef_accepted",
+           "partners_intervention"."unicef_court",
+           "partners_intervention"."unicef_review_type",
+           "partners_intervention"."humanitarian_flag",
+           "partners_intervention"."capacity_development",
+           "partners_intervention"."other_info",
+           "partners_intervention"."other_partners_involved",
+           "partners_intervention"."technical_guidance",
+           "partners_intervention"."cash_transfer_modalities",
+           "partners_intervention"."cancel_justification",
+           "partners_intervention"."date_partnership_review_performed",
+           "partners_intervention"."accepted_on_behalf_of_partner",
+           "partners_intervention"."activation_protocol",
+           "partners_intervention"."confidential",
+           "partners_intervention"."has_activities_involving_children",
+           "partners_intervention"."has_data_processing_agreement",
+           "partners_intervention"."has_special_conditions_for_construction",
+           "partners_intervention"."final_review_approved",
+           "partners_intervention"."other_details",
+           "partners_intervention"."partner_authorized_officer_signatory_id"
+    FROM "partners_intervention"
+    WHERE   ( "partners_intervention"."id" IN ( ##LIST OF "action_points_actionpoint"."intervention_id" in action_points_actionpoint for the page ## )
+              OR  partners_intervention"."id" IN  ( ##LIST OF  "audit_engagement_active_pd"."intervention_id" in audit_engagement_active_pd for the page ## )
+            );
+
+    """
+
     def get_content_type(self, sub_type):
         mapping = {
             AuditAudit: "audit",
